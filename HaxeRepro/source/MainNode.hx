d860ebbb4c86a8cc8c63efbdaefbb3241e9df5a8
Axonn (Kyliathy) Echysttas <20860173+Kyliathy@users.noreply.github.com>
2020-09-19 02:37:09 +0200

    feat: added libraries for node: express, luxon, source map support feat: error messages in node now have proper source maps feat: added the awesome dts2hx library. You must use it to generate haxe externs

--- HaxeRepro/source/MainNode.hx
+++ HaxeRepro/source/MainNode.hx
@@ -1,9 +1,11 @@
 import js.Node.console;
+import luxon.DateTime;
 
 class MainNode {
 	static var i:Int = 0;
 
 	static function main() {
+		SourceMapSupport.install();
 		// Configure our HTTP server to respond with Hello World to all requests.
 		console.log("Start server now!");
 		var server = js.node.Http.createServer(function(request, response) {
@@ -11,7 +13,7 @@ class MainNode {
 
 			var h:Human = new Human("Bob", 11, false, "Fisherman");
 
-			console.log("hai salut because zzsss" + request.url);
+			console.log("hai salut because url was " + request.url);
 			response.end("Hello Human " + h);
 		});
 
@@ -20,5 +22,14 @@ class MainNode {
 
 		// Put a console.log on the terminal
 		trace("Server running at 127.0.0.1:8000");
+
+		var app = Express.call();
+		var port = 3000;
+		app.get('/', (req, res, next) -> {
+			var h:Human = new Human("Bob", 11, false, "Fisherman");
+			var cucu:DateTime = DateTime.local();
+			res.send('Hellossz Worlda!  ' + h + " on1 " + cucu.minute);
+		});
+		app.listen(port, () -> trace('Example app listening at http://localhost:${port}'));
 	}
 }
